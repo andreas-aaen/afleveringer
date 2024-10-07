ea74045a76a8ebebcbdb3a0604f7c533c5927679
andreas-aaen <aaaen24@student.aau.dk>
2024-10-07 12:17:46 +0200

    kommentarer

--- ur.py
+++ ur.py
@@ -13,7 +13,7 @@ while True:
     h = currentTime.hour
     m = currentTime.minute
     s = currentTime.second
-    ms = currentTime.microsecond / 1000
+    ms = currentTime.microsecond / 1000     # konvertér microsekunder til millisekunder
     
     screen.fill((0, 0, 0))
 
@@ -36,15 +36,24 @@ while True:
         pygame.draw.line(screen, (255, 255, 255), startPoint, endPoint, 5)
         angle += 30
     pygame.draw.circle(screen, (0, 0, 0) , startPoint, 270)
+    
+    """
+
+    h, m, s, ms == variabler defineret med datetime.now()
+    (h % 12 + m / 60), (m + s / 60), osv. udglatter viserenes bevægelse
+    % sikrer 'wrapping' af timeviseren fra kl. 13 til 24
+    -90 sikrer at uret starter udregningerne fra kl. 12 og ikke kl. 3, da vinklerne starter fra 'øst'
+
+    """
 
     # udregn timeviserens vinkel og tegn
-    hourAngle = ((360 / 12) * (h % 12 + m / 60)) - 90       # h == currentTime.hour
+    hourAngle = ((360 / 12) * (h % 12 + m / 60)) - 90
     endOffset = [250 * math.cos(math.radians(hourAngle)), 250 * math.sin(math.radians(hourAngle))]
     endPosition = (startPoint[0] + endOffset[0], startPoint[1] + endOffset[1])
     pygame.draw.line(screen, (210, 40, 40), startPoint, endPosition, 10)
 
     # udregn minutviserens vinkel og tegn
-    minuteAngle = ((360 / 60) * (m % 60 + s / 60)) - 90     # m == currentTime.minute, s == currentTime.second
+    minuteAngle = ((360 / 60) * (m + s / 60)) - 90
     endOffset = [250 * math.cos(math.radians(minuteAngle)), 250 * math.sin(math.radians(minuteAngle))]
     endPosition = (startPoint[0] + endOffset[0], startPoint[1] + endOffset[1])
     pygame.draw.line(screen, (210, 40, 40), startPoint, endPosition, 4)
@@ -55,7 +64,7 @@ while True:
     endPosition = (startPoint[0] + endOffset[0], startPoint[1] + endOffset[1])
     pygame.draw.line(screen, (255, 255, 255), startPoint, endPosition, 2)
 
-    # gem startpunkterne bag en cirkel
+    # gem visernes startpunkterne bag en cirkel
     pygame.draw.circle(screen, (210, 40, 40), startPoint, 10)
 
     pygame.display.flip()
